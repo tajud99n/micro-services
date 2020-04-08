5af0c867c08775593060ce1fdee5adb06004a90a
tajud99n <giwa208@gmail.com>
2020-04-08 16:18:21 +0100

    episode3and4

--- products-api/data/products.go
+++ products-api/data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -17,15 +18,53 @@ type Product struct {
 	DeletedOn   string  `json:"-"`
 }
 
+var ErrProductNotFound = fmt.Errorf("product not found")
+
 type Products []*Product
+
+func (p *Product) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
+}
 
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
 }
 
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
 func GetProducts() Products {
 	return productList
+}
+
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	productList[pos] = p
+
+	return nil
+}
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
 }
 
 var productList = []*Product{
--- products-api/handlers/products.go
+++ products-api/handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/tajud99n/nic/products-api/data"
 )
@@ -20,17 +22,76 @@ func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	case http.MethodGet:
 		p.getProducts(rw, r)
 		break
+	case http.MethodPost:
+		p.addProduct(rw, r)
+		break
+	case http.MethodPut:
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		p.updateProduct(id, rw, r)
+		break
 	default:
 		rw.WriteHeader(http.StatusNotFound)
 	}
 }
 
 func (p *Product) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("get")
 	lp := data.GetProducts()
 
 	err := lp.ToJSON(rw)
 	if err != nil {
-		http.Error(rw, "error marshalling product", http.StatusInternalServerError)
+		http.Error(rw, "error marshalling products", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (p *Product) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("post")
+	np := &data.Product{}
+
+	err := np.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "error unmarshalling product", http.StatusBadRequest)
+		return
+	}
+	data.AddProduct(np)
+}
+
+func (p *Product) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("put")
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "error unmarshalling product", http.StatusBadRequest)
+		return
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "server error", http.StatusInternalServerError)
 		return
 	}
 }
