a3b03db2c53d548d4b185cd1712a5d90ccaa70a3
tajud99n <giwa208@gmail.com>
2020-04-12 07:17:55 +0100

    cors implementation

--- products-api/main.go
+++ products-api/main.go
@@ -8,6 +8,7 @@ import (
 	"os/signal"
 	"time"
 
+	gorillaHandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/tajud99n/nic/products-api/handlers"
 )
@@ -30,9 +31,12 @@ func main() {
 	postRouter.HandleFunc("/", ph.AddProduct)
 	postRouter.Use(ph.MiddlewareProductValidation)
 
+	// CORS
+	ch := gorillaHandlers.CORS(gorillaHandlers.AllowedOrigins([]string{"*"}))
+
 	s := &http.Server{
 		Addr:         ":9000",
-		Handler:      sm,
+		Handler:      ch(sm),
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
